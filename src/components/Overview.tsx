ae72d99e3c2af41c42c8da639f82a92423a488d0
Yannick Lang <layaxx@gmx.de>
2021-12-09 16:52:06 +0100

    add overview page

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom"
+
 export default function Overview() {
-  return null
+  return (
+    <>
+      <h2>Currently implemented Modules</h2>
+      <ol>
+        <li>
+          Binary Trees
+          <ul>
+            <li>
+              <Link to="/btree/theory">Theory</Link>
+            </li>
+            <li>
+              <Link to="/playground/btree">Visual Playground</Link>
+            </li>
+          </ul>
+        </li>
+      </ol>
+    </>
+  )
 }
